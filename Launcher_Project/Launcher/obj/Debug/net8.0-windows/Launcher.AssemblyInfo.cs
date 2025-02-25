a13d6d0dd1dc1106ae6224b82c6c046d51c31544
CHAPELLIERE Youenn <y.chapelliere@rns.ecolescreatives.com>
2025-02-25 13:29:45 +0100

    [ UPDATE ] début d'UI

--- Launcher_Project/Launcher/obj/Debug/net8.0-windows/Launcher.AssemblyInfo.cs
+++ Launcher_Project/Launcher/obj/Debug/net8.0-windows/Launcher.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Launcher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4878dd78824e38bb9d83ea6dfff86cc4234fab0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Launcher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Launcher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
